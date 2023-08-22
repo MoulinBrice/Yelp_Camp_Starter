d1081f2499478c1cde1e578447fba9e5a73fa6ac
brice MOULIN <moulin_brice@hotmail.com>
2023-08-22 18:18:43 -0400

    Création de la navbar et de la page des camps
    Signed-off-by: brice MOULIN <moulin_brice@hotmail.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { CampsComponent } from './pages/camps/camps.component';
 
 const routes: Routes = [
   { path: '',   redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
+  { path: 'camps', component: CampsComponent },
+  { path: '**', redirectTo: 'home' }
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CampsComponent } from './pages/camps/camps.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavbarComponent,
+    CampsComponent
   ],
   imports: [
     BrowserModule,
